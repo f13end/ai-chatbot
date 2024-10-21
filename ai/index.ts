7b881241baa7fe5a0a364fd65c93bf80ed67182a
f13end <3438@pm.me>
2024-10-21 18:26:49 +0300

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -3,6 +3,6 @@ import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
 export const customModel = wrapLanguageModel({
-  model: openai("gpt-4o-gpt-4o-mini-2024-07-18"),
+  model: openai("gpt-4o-mini"),
   middleware: customMiddleware,
 });
